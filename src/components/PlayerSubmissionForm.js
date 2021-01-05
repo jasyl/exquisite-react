bd15a86a6f6328b973db5ce193dd1b9c5213adfe
Jasmine <jasylop@gmail.com>
2021-01-05 12:53:01 -0800

    added the input tags and updated the submit handler to pass in the new line to the callback function and reset the text boxes

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,21 +3,92 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+  const [line, setLine] = useState({
+    adjective1: '',
+    noun1: '',
+    adverb: '',
+    verb: '',
+    adjective2: '',
+    noun2: '',
+  });
+
+  const onFormChange = (e) => {
+    setLine({
+      ...line, 
+      [e.target.name]: e.target.value})
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    props.sendSubmission(line);
+
+    setLine({
+      adjective1: '',
+      noun1: '',
+      adverb: '',
+      verb: '',
+      adjective2: '',
+      noun2: '',
+    });
+
+  }
+
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{ props.index }</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form className="PlayerSubmissionForm__form" onSubmit={onSubmitHandler} >
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
+          <span>The</span>
+
           <input
-            placeholder="hm..."
-            type="text" />
+            name="adjective1"
+            placeholder="adjective"
+            type="text"
+            onChange={onFormChange}
+            value={line.adjective1} />
+
+          <input
+            name="noun1"
+            placeholder="noun"
+            type="text" 
+            onChange={onFormChange}
+            value={line.noun1} />
+
+          <input
+            name="adverb"
+            placeholder="adverb"
+            type="text"
+            onChange={onFormChange}
+            value={line.adverb} />
+
+          <input
+            name="verb"
+            placeholder="verb"
+            type="text"
+            onChange={onFormChange} 
+            value={line.verb}/>
+
+          <span>the</span>
+
+          <input
+            name="adjective2"
+            placeholder="adjective"
+            type="text"
+            onChange={onFormChange}
+            value={line.adjective2} />
+          <input
+            name="noun2"
+            placeholder="noun"
+            type="text"
+            onChange={onFormChange}
+            value={line.noun2} />
+
+          <span>.</span>
 
         </div>
 
