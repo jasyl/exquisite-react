0527fd901181ccc6a7ae6c73dd62ff021dfb0353
Jasmine <jasylop@gmail.com>
2021-01-05 16:55:17 -0800

    created a function to incorporate dynamic fields and generate input and spans as necessary

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
+
 const PlayerSubmissionForm = (props) => {
-  const [line, setLine] = useState({
-    adjective1: '',
-    noun1: '',
-    adverb: '',
-    verb: '',
-    adjective2: '',
-    noun2: '',
-  });
+
+  const setInitialState = () => {
+    let initialState = {};
+    props.fields.forEach((field) => {
+      if (field.key) initialState[field.key] = ''; 
+  })
+    return initialState
+  }
+
+  const [line, setLine] = useState(setInitialState());
 
   const onFormChange = (e) => {
     setLine({
@@ -24,16 +27,37 @@ const PlayerSubmissionForm = (props) => {
 
     props.sendSubmission(line);
 
-    setLine({
-      adjective1: '',
-      noun1: '',
-      adverb: '',
-      verb: '',
-      adjective2: '',
-      noun2: '',
-    });
+    setLine(setInitialState());
 
   }
+
+  const isValid = (value) => {
+    if (value === '') return 'PlayerSubmissionFormt__input--invalid'
+    else return null
+  }
+
+  const generateInputFields = () => {
+    return (
+      props.fields.map((field, i) => {
+        if (field.key) {
+          return (
+            <input
+              key={field.key}
+              name={field.key}
+              placeholder={field.placeholder}
+              type="text"
+              onChange={onFormChange}
+              value={line[field.key]}
+              className={isValid(line[field.key])}
+            />
+          )
+        } else {
+          return <span key={field}>{field}</span>
+        }
+      }
+    ))
+  }
+
 
   return (
     <div className="PlayerSubmissionForm">
@@ -43,52 +67,61 @@ const PlayerSubmissionForm = (props) => {
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
-          <span>The</span>
+          {generateInputFields()}
+
+          {/* <span>The</span>
 
           <input
             name="adjective1"
-            placeholder="adjective"
+            placeholder="adjective1"
             type="text"
             onChange={onFormChange}
-            value={line.adjective1} />
+            value={line.adjective1} 
+            className={isValid(line.adjective1)}
+            />
 
           <input
             name="noun1"
-            placeholder="noun"
+            placeholder="noun1"
             type="text" 
             onChange={onFormChange}
-            value={line.noun1} />
+            value={line.noun1}
+            className={isValid(line.adjective1)} />
 
           <input
             name="adverb"
             placeholder="adverb"
             type="text"
             onChange={onFormChange}
-            value={line.adverb} />
+            value={line.adverb}
+            className={isValid(line.adjective1)} />
 
           <input
             name="verb"
             placeholder="verb"
             type="text"
             onChange={onFormChange} 
-            value={line.verb}/>
+            value={line.verb}
+            className={isValid(line.adjective1)}/>
 
           <span>the</span>
 
           <input
             name="adjective2"
-            placeholder="adjective"
+            placeholder="adjective2"
             type="text"
             onChange={onFormChange}
-            value={line.adjective2} />
+            value={line.adjective2}
+            className={isValid(line.adjective1)} />
           <input
             name="noun2"
-            placeholder="noun"
+            placeholder="noun2"
             type="text"
             onChange={onFormChange}
-            value={line.noun2} />
+            value={line.noun2}
+            className={isValid(line.noun2)} />
 
-          <span>.</span>
+          <span>.</span> */}
 
         </div>
 
